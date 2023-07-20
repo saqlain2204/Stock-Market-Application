d6dd25ffac2ff18d27123ea5912204e4be3f4553
M Saqlain <saqlain2204@gmail.com>
2023-07-20 19:49:23 +0530

    Updated the Refresh Button Position

--- app.py
+++ app.py
@@ -62,55 +62,60 @@ left_co, cent_co,last_co = st.columns(3)
 with cent_co:
     st.image('logo.png')
 
-st.markdown("# Welcome to our financial DashBoard.\n ###### This application is currently in its Beta version/testing period.")
+st.markdown("<h1 style='text-align: center; color: #7fc3f7;'>Welcome to FinTrack</h1>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center;'>This application is currently in its Beta version/testing period.</p>", unsafe_allow_html=True)
 
 ## Main page function
 def main_page(company, start_date, end_date, interval, period, stock):
-    refresh=st.button("Refresh")
-    ## Refresh data on click
-    if(refresh or not refresh):
-        try:
-            ticker = yf.download(company, start=start_date, end=end_date,  interval=interval, period=period)  
 
-            ## Columns:
-            High_col, low_col = st.columns(2, gap="medium")
+    try:
+        ticker = yf.download(company, start=start_date, end=end_date,  interval=interval, period=period)  
 
-            ## A column which contains the highest price of the stock in the given period
-            with High_col:
-                st.title(f"Highest")
-                High=ticker["High"].max()
-                st.markdown(f"##### :green[{High}]")
+        ## Columns:
+        High_col, low_col, button_col = st.columns(3, gap="medium")
 
-            ## A columns which contains the lowest price of the stock in the given period
-            with low_col:                                                       
-                st.title(f"Lowest")
-                Low=ticker["Low"].min()
-                st.markdown(f"##### :red[{Low}]")
+        ## A column which contains the highest price of the stock in the given period
+        with High_col:
+            st.title(f"Highest")
+            High=ticker["High"].max()
+            st.markdown(f"##### :green[{High}]")
 
-            ## Displaying DataFrame
-            st.dataframe(ticker.iloc[::-1], use_container_width=True)
+        ## A columns which contains the lowest price of the stock in the given period
+        with low_col:                                                       
+            st.title(f"Lowest")
+            Low=ticker["Low"].min()
+            st.markdown(f"##### :red[{Low}]")
 
-            graph, balance_sheet, news = st.tabs(["Statistics", "Balance sheet","News"])
+        ## Refresh Button
+        with button_col:
+            st.markdown(" \n")
+            refresh=st.button("Refresh")
+        if(refresh):
+            ticker = yf.download(company, start=start_date, end=end_date,  interval=interval, period=period)
 
-            with graph:
-                ## Graphs:
-                var= st.selectbox("Select the dependent variable",("Open","High","Low","Adj Close","Volume"))
-                st.plotly_chart(px.line(data_frame=ticker, y=ticker[var]), use_container_width=True)
+        ## Displaying DataFrame
+        st.dataframe(ticker.iloc[::-1], use_container_width=True)
 
-            with balance_sheet:
-                ## Balance Sheet
-                st.dataframe(stock.balance_sheet, use_container_width=True)
+        graph, balance_sheet, news = st.tabs(["Statistics", "Balance sheet","News"])
 
-            with news:
-                for i in range(5):
-                    new=stock.news[i]['title']
-                    link=stock.news[i]['link']
-                    st.write(f"{new}: \n {link}")
+        with graph:
+            ## Graphs:
+            var= st.selectbox("Select the dependent variable",("Open","High","Low","Adj Close","Volume"))
+            st.plotly_chart(px.line(data_frame=ticker, y=ticker[var]), use_container_width=True)
 
-    
-        except:
-            ## Throws warning if any error pops up
-            st.warning("Enter a valid ticker name in the sidebar to generate Data")     
+        with balance_sheet:
+            ## Balance Sheet
+            st.dataframe(stock.balance_sheet, use_container_width=True)
+
+        with news:
+            for i in range(5):
+                new=stock.news[i]['title']
+                link=stock.news[i]['link']
+                st.write(f"{new}: \n {link}")
+
+    except:
+        ## Throws warning if any error pops up
+        st.warning("Enter a valid ticker name in the sidebar to generate Data")     
 
 def main():
 
